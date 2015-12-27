0453d2f103f32b0902cfcbb8c9c8d7837822ae83
avidtraveller <thil@foresightcreations.com>
2015-12-27 19:13:55 +1100

    Use geohash

--- mix.exs
+++ mix.exs
@@ -28,6 +28,6 @@ defmodule Geocoder.Mixfile do
      {:poison, "~> 1.4"},
      {:towel, "~> 0.2"},
      {:poolboy, "~> 1.5"},
-     {:geohash, github: "treetopllc/geohash"}]
+     {:geohash, "~> 0.1"}]
   end
 end
